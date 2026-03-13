35192ba67b560854cd193c733684e163df3af76e
NathanaelOsborne <yudi0sath@gmail.com>
2026-03-13 09:55:26 +0700

    Update appv3.py

--- appv3.py
+++ appv3.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1EGIfRSCWdeCCpfqqizwWshQSVlUunaJd
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 
